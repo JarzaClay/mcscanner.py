733d07d6f8cbbcb1aeaf265d0f853034a0438345
Jarza <96431656+JarzaClay@users.noreply.github.com>
2022-11-13 20:19:48 -0500

    Added player name support

--- mcscanner.py
+++ mcscanner.py
@@ -72,14 +72,14 @@ def print_time(threadName):
         except:
             print("Failed to get status of: " + ip)
         else:
-            print("Found server: " + ip + " " + status.version.name + " " + str(status.players.online) + " Players online")
+            print("Found server: " + ip + " " + status.version.name + " " + str(status.players.online) + " Players online, Names of players: " + {(query.players.names)})
             if searchterm in status.version.name:
                 with open(outputfile) as f:
                     if ip not in f.read():
                         with open(publicserverlist) as g:
                             if ip not in g.read():
                                 text_file = open(outputfile, "a")
-                                text_file.write(ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online) + " Players online")
+                                text_file.write(ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online) + " Players online, Names of players: " + {(query.players.names)})
                                 text_file.write(os.linesep)
                                 text_file.close()
 
